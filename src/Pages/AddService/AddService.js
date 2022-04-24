cd686796638165ed75d79e27979c293561c293b4
Mehedi4467 <mehedihassan4467@gmail.comm>
2022-04-24 20:10:35 +0600

    load option and post service

--- src/Pages/AddService/AddService.js
+++ src/Pages/AddService/AddService.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from "react-hook-form";
 
 const AddService = () => {
     const { register, handleSubmit } = useForm();
-    const onSubmit = data => console.log(data);
+
+    const [loading, setLoading] = useState(false);
+
+
+
+    const onSubmit = data => {
+        console.log(data);
+        setLoading(true);
+        const url = `http://localhost:5000/service`;
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+
+        })
+            .then(res => res.json())
+            .then(result => {
+                console.log(result);
+                if (result.acknowledged) {
+                    setLoading(false);
+                }
+
+            })
+    };
     return (
         <div className='w-50 mx-auto'>
             <h1>This is add Service data</h1>
@@ -13,7 +38,13 @@ const AddService = () => {
                 <textarea placeholder='Description' className='my-4' {...register("description")} />
                 <input placeholder='Price' type="number" {...register("price")} />
                 <input placeholder='Photo URL' type="text" {...register("img")} />
-                <input className='mt-4' type="submit" />
+
+                {
+                    loading ? <div className="spinner-grow mx-auto mt-4" role="status">
+
+                    </div> : <input className='mt-4' type="submit" />
+                }
+
             </form>
         </div>
     );
