8ca42d7257429c503cf11bf684e0f05d03ff1c06
Mehedi4467 <mehedihassan4467@gmail.comm>
2022-04-24 19:40:03 +0600

    react form hook added

--- src/Pages/AddService/AddService.js
+++ src/Pages/AddService/AddService.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { useForm } from "react-hook-form";
 
 const AddService = () => {
+    const { register, handleSubmit } = useForm();
+    const onSubmit = data => console.log(data);
     return (
-        <div>
+        <div className='w-50 mx-auto'>
             <h1>This is add Service data</h1>
+
+            <form className='d-flex flex-column' onSubmit={handleSubmit(onSubmit)}>
+                <input placeholder='Service Name'  {...register("name", { required: true, maxLength: 20 })} />
+                <textarea placeholder='Description' className='my-4' {...register("description")} />
+                <input placeholder='Price' type="number" {...register("price")} />
+                <input placeholder='Photo URL' type="text" {...register("img")} />
+                <input className='mt-4' type="submit" />
+            </form>
         </div>
     );
 };
